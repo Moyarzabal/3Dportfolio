3ae2dc8aa1ab5f1246b37f1bd6f9195d8cfeb3dc
Moyarzabal <hanshun1018@g.ecc.u-tokyo.ac.jp>
2025-07-16 22:10:02 +0900

    first commit

---
+++ src/components/canvas/index.js
@@ -0,0 +1,8 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computers";
+import CameramanCanvas from "./Cameraman";
+import GroundCanvas from "./Ground";
+import StarsCanvas from "./Stars";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, CameramanCanvas, GroundCanvas, StarsCanvas };
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from "./Hero";
+import Hero2 from "./Hero2";
+
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+import ShowcaseSection from "./ShowcaseSection";
+
+export {
+  Hero,
+  Hero2,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas,
+  ShowcaseSection
+};
